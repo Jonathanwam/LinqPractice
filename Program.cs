17aa00412ffa9ba61b6ae821ae8e6b4a1bbb6601
Jonathan Wambach <jonathanwam@gmail.com>
2018-07-26 16:37:20 -0400

    Added delegate demos

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace linqpractice
 {
     class Program
     {
+        public delegate string Reverse(String s);
         public static void Main(string[] args) 
         {
 
@@ -44,7 +45,77 @@ namespace linqpractice
         Console.WriteLine("List of all names:");
         foreach(var n in namesOnly)
             Console.Write(n.fName + ", ");
+
+            
+        Console.Write("\n---------------------------\n");
+
+        // simple delegate
+
+        String line = "This is a simple string";
+        Console.WriteLine("Simple Delegate Demo");
+        Reverse rev = ReverseString;
+        Console.WriteLine(line);
+        Console.WriteLine("Reversed ...");
+        Console.WriteLine(rev(line));
+
+        Console.Write("\n---------------------------\n");
+        Console.Write("Running Anonymous Delegate Demo\n");
+        AnonymousDelegateDemo();
+
+        Console.Write("\n---------------------------\n");
+        Console.Write("Running Lambda Delegate Demo\n");
+        LambdaDelegateDemo();
         }
 
+        static string ReverseString(string s)
+        {
+            var result = new string(s. Reverse().ToArray());
+            return result;
+        }
+
+        private static void AnonymousDelegateDemo()
+        {
+            List<int> list = new List<int>();
+
+            for(int i = 1; i <= 100; i++)
+            {
+                list.Add(i);
+            }
+
+            // Anon delegate
+            List<int> result = list.FindAll(
+                delegate (int num)
+                {
+                    return (num % 2 == 0);
+                }
+            );
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void LambdaDelegateDemo()
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                list.Add(i);
+            }
+
+            // Lambda expression (or just called Lambda)
+            // Core building block of LINQ
+            // First introduced to C# 3.0
+            List<int> result = list.FindAll(i => i % 2 == 0);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        
     }
 }
